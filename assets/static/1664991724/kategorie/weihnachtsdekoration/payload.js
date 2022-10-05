7fc8d9a1a90fecfc6866a31d4c64b227644a8d6d
outdoorhosen24-de <outdoorhosen24-de@users.noreply.github.com>
2022-10-05 23:34:45 +0000

    deploy: 31d930867950c543dcf1b1b7ce7595264496fe8e

--- assets/static/1664991724/kategorie/weihnachtsdekoration/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/kategorie/weihnachtsdekoration", {data:[{_img:{"/_ipx/f_webp,q_90,s_407x229/assets/images/images/teelichter-100er-flatpack-weisz-sonstige-6045405-1.jpg":"\u002Fassets\u002Fimages\u002Fteelichter-100er-flatpack-weisz-sonstige-6045405-1.webp"}}],fetch:{},mutations:void 0});
